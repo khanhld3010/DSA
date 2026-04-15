a2550caf3119c3568719d4dfbcde99e8456856c3
khanhld3010 <luuduckhanh3010@gmail.com>
2026-04-15 14:41:20 +0700

    checkBracket

--- DSA/src/checkBracket/checkBracket.java
+++ DSA/src/checkBracket/checkBracket.java
@@ -4,53 +4,48 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class checkBracket {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         String str1 = "s * (s – a) * (s – b) * (s – c)";    //? true
         String str2 = "(– b + (b2 – 4*a*c)^0.5) / 2*a";     //? true
         String str3 = "s * (s – a) * (s – b * (s – c)";     //? false
         String str4 = "s * (s – a) * s – b) * (s – c)";     //? false
-        String str5 = "(– b + (b^2 – 4*a*c)^(0.5/ 2*a))";   //? false
-
-        boolean checkBracket1 =  checkBracketString(str1);
-        display(checkBracket1,1);
-        boolean checkBracket2 =  checkBracketString(str2);
-        display(checkBracket2,2);
-        boolean checkBracket3 =  checkBracketString(str3);
-        display(checkBracket3,3);
-        boolean checkBracket4 =  checkBracketString(str4);
-        display(checkBracket4,4);
-        boolean checkBracket5 =  checkBracketString(str5);
-        display(checkBracket5,5);
-
-
+        String str5 = "(– b + (b^2 – 4*a*c)^(0.5/ 2*a))";   //? true
+        boolean checkBracket1 = checkBracketString(str1);
+        display(checkBracket1, 1);
+        boolean checkBracket2 = checkBracketString(str2);
+        display(checkBracket2, 2);
+        boolean checkBracket3 = checkBracketString(str3);
+        display(checkBracket3, 3);
+        boolean checkBracket4 = checkBracketString(str4);
+        display(checkBracket4, 4);
+        boolean checkBracket5 = checkBracketString(str5);
+        display(checkBracket5, 5);
     }
-    static void display(boolean checkBracket, int number){
-        if (checkBracket){
-            System.out.println("biểu " +number+ " thức đúng ngoặc");
-        }else {
-            System.out.println("biểu " +number+ " thức không đúng ngoặc");
+
+    static void display(boolean checkBracket, int number) {
+        if (checkBracket) {
+            System.out.println("biểu " + number + " thức đúng ngoặc");
+        } else {
+            System.out.println("biểu " + number + " thức không đúng ngoặc");
         }
     }
 
     static boolean checkBracketString(String str) {
-        Stack<String> stackStr = new Stack<>();
-        String[] arrString = str.split("");
+        Stack<Character> stackStr = new Stack<>();
+        char[] arrString = str.toCharArray();
         System.out.println(Arrays.toString(arrString));
-        for (String s : arrString) {
-            if (s.equals("(")) {
+        for (char s : arrString) {
+            if (s == '(') {
                 stackStr.push(s);
             }
-            if (s.equals(")")){
-                if (stackStr.size() == 0){
+            if (s == ')') {
+                if (stackStr.isEmpty()) {
                     return false;
-                }else {
+                } else {
                     stackStr.pop();
                 }
             }
         }
-        if (stackStr.size() != 0){
-            return false;
-        }
-        return true;
+        return stackStr.isEmpty();
     }
 }
